bac99df6a43b922d7d0433b44203148a6f0cbf47
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-08-15 04:42:31 -0700

    adding basic type concept examples

---
+++ types/anotations/typeanotation.ts
@@ -0,0 +1,9 @@
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
+
+let nothing: null = null;
+let undifined: undefined = undefined;
+
+// buult in objects
+let now: Date = new Date();
