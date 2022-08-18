75481aa0027881380e552fd62a845f222d0f6e7c
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-08-18 01:44:10 -0700

    added Funtions anotations

---
+++ types/anotations/functions.ts
@@ -0,0 +1,45 @@
+const add = (a: number, b: number) => {
+  return a + b;
+};
+
+const substract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string) => {
+  throw new Error(message);
+};
+
+const todaysWeather = {
+  date: new Date(),
+  weather: "sunny"
+};
+
+const logWeather = (todaysWeather: { date: Date, weather: string }): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+const weather = ({date, weather} {date: Date, weather: string}): void => {
+  console.log(date);
+  console.log(weather);
+}
+// ES2015
+const weatherLog = ({ date, weather }) => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
--- types/anotations/variables.ts
+++ types/anotations/variables.ts
@@ -1,5 +1,9 @@
-const items: number = 5;
-let speed: string = "100Mph";
+// const items: number = 5; // Type anotation
+const items = 5; // short hand
+
+// let speed: string = "100Mph"; // Type anotation
+let speed = "100Mph"; // short hand
+
 speed = "150Mph";
 speed = "too fast";
 
@@ -31,3 +35,37 @@ let point: { x: number; y: number } = {
   x: 10,
   y: 20,
 };
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json); // {x: 10, y: 20};
+
+// 2) When we declare a variable on one line and initalizate it later
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    foundWord = true;
+  }
+}
+
+// 3) Veriable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  } else {
+    numberAboveZero = false;
+  }
+}
+
+//
